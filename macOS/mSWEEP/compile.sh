fbbf130851a5d5121188435bc7e67ad5aabec8fe
Tommi Mäklin <tommi@maklin.fi>
2023-09-04 14:41:19 +0300

    Use a docker container (shepherdjerred/macos-cross-compiler) to build mSWEEP for macOS.

--- macOS/mSWEEP/compile.sh
+++ macOS/mSWEEP/compile.sh
@@ -1,1 +1,13 @@
-./build.sh $1
+set -euxo pipefail
+
+cp ../$2-toolchain.cmake ./
+
+docker run \
+  -v `pwd`:/io \
+  --rm \
+  -it \
+  ghcr.io/shepherdjerred/macos-cross-compiler:latest \
+  /bin/bash /io/build2.sh $1 $2
+
+rm $2-toolchain.cmake
+
