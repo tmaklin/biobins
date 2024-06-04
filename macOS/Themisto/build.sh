aa2b071383eb18a345e2d1054ce12d13b43573e6
Tommi Mäklin <tommi@maklin.fi>
2024-06-04 23:02:54 +0300

    Fix issues with Themisto for aarch64 mac

--- macOS/Themisto/build.sh
+++ macOS/Themisto/build.sh
@@ -47,6 +47,7 @@ if [ "$ARCH" = "x86-64" ]; then
     export CC="x86_64-apple-darwin22-gcc"
     export CXX="x86_64-apple-darwin22-g++"
 
+    ## Setup ggcat-cpp-api cargo config files for cross compilation
     sed "s/cargo build/RUSTFLAGS='-L \/osxcross\/SDK\/MacOSX13.0.sdk\/usr\/lib' cargo build --target x86_64-apple-darwin/g" ../ggcat/crates/capi/ggcat-cpp-api/Makefile | sed 's/target\/release/target\/x86_64-apple-darwin\/release/g' | sed 's/fPIE/fPIE -march=x86-64 -mtune=generic -m64 -fPIC/g' | sed 's/ar cr/\/gcc\/bin\/x86_64-apple-darwin22-gcc-ar cr/g' > Makefile.tmp
     mv Makefile.tmp ../ggcat/crates/capi/ggcat-cpp-api/Makefile
 
@@ -72,13 +73,27 @@ elif [ "$ARCH" = "arm64" ]; then
     export CC="aarch64-apple-darwin22-gcc"
     export CXX="aarch64-apple-darwin22-g++"
 
-    sed "s/cargo build/RUSTFLAGS='-L \/osxcross\/SDK\/MacOSX13.0.sdk\/usr\/lib' cargo build --target aarch64-apple-darwin/g" ../ggcat/crates/capi/ggcat-cpp-api/Makefile | sed 's/target\/release/target\/aarch64-apple-darwin\/release/g' | sed 's/fPIE/fPIE -march=x86-64 -mtune=generic -m64 -fPIC/g' | sed 's/ar cr/\/gcc\/bin\/aarch64-apple-darwin22-gcc-ar cr/g' > Makefile.tmp
+    ## Prevent KMC from compiling with x86 instructions
+    sed 's/^UNAME_S.*$/UNAME_S=Darwin/g' ../SBWT/KMC/Makefile | sed 's/^UNAME_M.*$/UNAME_M=arm64/g' | sed 's/^[[:space:]]*CC[[:space:]]*=.*$//g' > Makefile.tmp
+    sed 's/if(APPLE)/if(TRUE)/g' ../SBWT/KMC/CMakeLists.txt | sed 's/CMAKE_SYSTEM_PROCESSOR MATCHES "arm64"/TRUE/g' > CMakeLists.txt.tmp
+    mv Makefile.tmp ../SBWT/KMC/Makefile
+    mv CMakeLists.txt.tmp ../SBWT/KMC/CMakeLists.txt
+
+    ## Prevent sdsl-lite from building with native instructions
+    sed 's/-msse4.2[[:space:]]*-march=native/-march=armv8-a/g' ../SBWT/sdsl-lite/CMakeLists.txt > CMakeLists.txt.tmp
+    mv CMakeLists.txt.tmp ../SBWT/sdsl-lite/CMakeLists.txt
+
+    ## Setup ggcat-cpp-api cargo config files for cross compilation
+    sed "s/cargo build/RUSTFLAGS='-L \/osxcross\/SDK\/MacOSX13.0.sdk\/usr\/lib' cargo build --target aarch64-apple-darwin/g" ../ggcat/crates/capi/ggcat-cpp-api/Makefile | sed 's/target\/release/target\/aarch64-apple-darwin\/release/g' | sed 's/fPIE/fPIE -march=armv8-a -mtune=generic -m64 -fPIC/g' | sed 's/ar cr/\/gcc\/bin\/aarch64-apple-darwin22-gcc-ar cr/g' > Makefile.tmp
     mv Makefile.tmp ../ggcat/crates/capi/ggcat-cpp-api/Makefile
 
     # compile aarch64
     cmake -DCMAKE_TOOLCHAIN_FILE="/io/$ARCH-toolchain_GNU.cmake" \
           -DCMAKE_C_FLAGS="-march=armv8-a -mtune=generic -m64 -fPIC -fPIE" \
           -DCMAKE_CXX_FLAGS="-march=armv8-a -mtune=generic -m64 -fPIC -fPIE" \
+	  -DAPPLE=1 \
+	  -DCMAKE_SYSTEM_PROCESSOR="arm64" \
+	  -DROARING_DISABLE_NATIVE=ON \
           -DBZIP2_LIBRARIES="/osxcross/SDK/MacOSX13.0.sdk/usr/lib/libbz2.tbd" -DBZIP2_INCLUDE_DIR="/osxcross/SDK/MacOSX13.0.sdk/usr/include" \
           -DZLIB="/osxcross/SDK/MacOSX13.0.sdk/usr/lib/libz.tbd" -DZLIB_INCLUDE_DIR="/osxcross/SDK/MacOSX13.0.sdk/usr/include" \
 	  -DZLIB_LIBRARY=="/osxcross/SDK/MacOSX13.0.sdk/usr/lib/libz.tbd" \
